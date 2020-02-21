d904d4ac88dfe68c669ca101de36e5c4f23def69
danengh <40503559+danengh@users.noreply.github.com>
2020-02-21 08:47:01 -0600

    Update depSoftwarePrompt.sh
    added GPL license

--- depSoftwarePrompt.sh
+++ depSoftwarePrompt.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-# Created by Dan Engh
+
+# Short script meant to be run from Jamf that will prompt the user during
+# DEP enrollment.
+# Copyright (C) 2018  Dan Engh
 # Last modified Feb 8, 2018
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #Wait for user session
 userSession="No"
